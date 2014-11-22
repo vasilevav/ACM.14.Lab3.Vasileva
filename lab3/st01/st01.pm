c81a2a21f9bb13faa551584c3cf50f5bf35752f0
Ab1992ao <Andril999@mail.ru>
2014-11-22 23:19:01 +0400

    lab3
    Изменен интерфейс

--- lab3/st01/st01.pm
+++ lab3/st01/st01.pm
@@ -5,67 +5,95 @@ use CGI;
 use CGI::Carp qw(fatalsToBrowser);
 use DBI;
 use DBD::mysql;
-
 my $Myfile='lab2\st01\AbramovData';
-my @menu=
-(
-"Add object",
-"Change",
-"Delete",
-"Print all elements"
-);
-
 my @option_func=
 (
 \&add,
 \&delete,
-\&change,
-\&printall
+\&change
 );
 my @students=();
 sub st01
 {
 	my $q = new CGI;
-	my $st =0+$q->param("menu_");
+	my $st =$q->param("menu_");
 	my $global = {selfurl => $ENV{SCRIPT_NAME}, menu_=> $st};
 	#fromfile();
 	FromDB();
-	if($st && defined $option_func[$st-1])
-	{
-		$option_func[$st-1]->($q, $global);
-	}
+	if ($st eq "Add")
+		{
+			$option_func[0]->($q, $global);
+		}
+	elsif ($st eq "Delete")
+		{
+			$option_func[1]->($q, $global);
+		}
+	elsif ($st eq "Change") 
+		{
+			$option_func[2]->($q, $global);
+		}
 	else
 	{	
 		menu($q, $global);
 	}
 }
 
+sub PrintAll
+{
+	my($q, $global) = @_;
+	my $id=1;
+	print "<table border=\"1\">
+   <tr>
+	<th>ID</th>
+    <th>Name</th>
+    <th>Surname</th>
+    <th>Group</th>
+    <th>Age</th>
+	<th>TypeOfStudent</th>
+   </tr>";
+   foreach	my $item (@students)
+	{
+		print "<tr> 
+			<td>$id</td>
+			<td>$item->{name}</td>
+			<td>$item->{surname}</td> 
+			<td>$item->{group}</td> 
+			<td>$item->{age}</td>";
+		if(defined $item->{type})	
+			{ print "
+			<td>$item->{type}</td><td><INPUT TYPE='radio' NAME=\"number_\" value=$id></td></tr>";}
+		else 
+			{
+				print "<td></td><td><INPUT TYPE='radio' NAME=\"number_\" value=$id></td></tr>";
+			}	
+		$id++;
+	}
+	print "</table>";
+}
 
 sub menu
 {
 	my($q, $global) = @_;
+	my $id=1;
 	print $q->header('charset=windows-1251');
 	print "<HTML><HEAD><TITLE>Second Lab</TITLE></HEAD><BODY><hr><menu type=\"toolbar\"><ul type=\"disc\">";
-	print"<FORM>
-	<HR>Press a text: <br>
-	<P>Name of student:<BR><INPUT NAME=\"name_\" TYPE=TEXT maxsize=100><BR>
+	print "<FORM>
+	<HR>List of students. <br>";
+	PrintAll($q, $global);
+	 print "<P>Name of student:<BR><INPUT NAME=\"name_\" TYPE=TEXT maxsize=100><BR>
 	<P>Surname of student:<BR><INPUT NAME=\"surname_\" TYPE=TEXT maxsize=100><BR>
 	<P>Group of student:<BR><INPUT NAME=\"group_\" TYPE=TEXT maxsize=100><BR>
 	<P>Age of student:<BR><INPUT NAME=\"age_\" TYPE=TEXT maxsize=100><BR>
-	<P> Number of student:<BR><INPUT NAME=\"number_\" TYPE=TEXT maxsize=100><BR>
 	<INPUT TYPE='checkbox' NAME=\"starosta_\" VALUE=5>Steward<BR>
-	<INPUT TYPE='radio' NAME=\"menu_\" VALUE=1> Add<BR> 
-	<INPUT TYPE='radio' NAME=\"menu_\" VALUE=2> Del<BR>
-	<INPUT TYPE='radio' NAME=\"menu_\" VALUE=3> Change<BR>
-	<INPUT TYPE='radio' NAME=\"menu_\" VALUE=4> Print all<BR>
+	<INPUT TYPE='submit' NAME=\"menu_\" VALUE=Add>  <INPUT TYPE='submit' NAME=\"menu_\" VALUE=Delete>  <INPUT TYPE='submit' NAME=\"menu_\" VALUE=Change> 
 	<INPUT TYPE='HIDDEN' NAME='student' VALUE =\"1\"/>
-	<INPUT type='submit'> <INPUT type='reset'> <BR>";
+	<INPUT type='reset'> <BR>";
 	print "</BODY><BR><BR><a href=\"$global->{selfurl}\">Back to student list.</a><BR></FORM></HTML>";
 }
 sub TypeOfStudent
 {
 my($q) = @_;
-my $st=$q->param('starosta_');
+my $st=0+$q->param('starosta_');
 my $type;
 if($st==5)
 	{
@@ -81,6 +109,7 @@ sub add
 	my $group=$q->param('group_');
 	my $age=$q->param('age_');
 	my $type=TypeOfStudent($q);
+	my $k=0+$q->param('number_');
 	my $sthash=
 			{
 				name=>$name,
@@ -92,7 +121,12 @@ sub add
 	{	
 		$sthash->{type}=$type;
 	}
-	push(@students,$sthash);
+	if($k!=0)
+	{
+		$students[$k-1]=$sthash;
+	}
+	else 
+	{push(@students,$sthash);}
 	#tofile();
 	ToDB();
 	menu($q,$global);
@@ -115,46 +149,29 @@ sub change
 {
 	my($q, $global) = @_;
 	my $k=0+$q->param('number_');
-	my $type=TypeOfStudent($q);
+	my $hashref;
 	if (defined $students[$k-1])
-	{ 
-		my $hashref=$students[$k-1];
-		$hashref->{name}=$q->param("name_");
-		$hashref->{surname}=$q->param('surname_');
-		$hashref->{group}=$q->param('group_');
-		$hashref->{age}=$q->param('age_');
-		if($type eq "steward"){
-		$hashref->{type}=$type;}
-		#tofile();
+	{
+	$hashref=$students[$k-1];
+	print $q->header('charset=windows-1251');
+	print "<HTML><HEAD><TITLE>Second Lab</TITLE></HEAD><BODY><hr><menu type=\"toolbar\"><ul type=\"disc\">";
+	print "<FORM>
+	<HR>List of students. <br>";
+	PrintAll($q, $global);
+	 print "<P>Name of student:<BR><INPUT NAME=\"name_\" TYPE=TEXT maxsize=100 value=$hashref->{name}><BR>
+	<P>Surname of student:<BR><INPUT NAME=\"surname_\" TYPE=TEXT maxsize=100 value=$hashref->{surname}><BR>
+	<P>Group of student:<BR><INPUT NAME=\"group_\" TYPE=TEXT maxsize=100 value=$hashref->{group}><BR>
+	<P>Age of student:<BR><INPUT NAME=\"age_\" TYPE=TEXT maxsize=100 value=$hashref->{age}><BR>
+	<INPUT TYPE='checkbox' NAME=\"starosta_\" VALUE=5>Steward<BR>
+	<INPUT TYPE='submit' NAME=\"menu_\" VALUE=Add>  <INPUT TYPE='submit' NAME=\"menu_\" VALUE=Delete>  <INPUT TYPE='submit' NAME=\"menu_\" VALUE=Change> 
+	<INPUT TYPE='HIDDEN' NAME='student' VALUE =\"1\"/>
+	<INPUT TYPE='HIDDEN' NAME='number_' VALUE =\"$k\"/>
+	<INPUT type='reset'> <BR>";
+	print "</BODY><BR><BR><a href=\"$global->{selfurl}\">Back to student list.</a><BR></FORM></HTML>";
 		ToDB();
 	}
-	menu($q,$global);	
 }
 
-sub printall
-{	
-	my ($q,$global) = @_;
-	print $q->header('charset=windows-1251');
-	print "<pre>\n------------------------------\n <ol value=1>";
-	foreach	my $item (@students)
-	{
-		
-		print "<li>Student: 
-			Name:$item->{name} 
-			Surname:$item->{surname} 
-			Group:$item->{group} 
-			Age:$item->{age}";
-		if(defined $item->{type})	
-			{ print "
-			TypeOfStudent: $item->{type}</li>";}
-		else 
-			{
-				print "</li>";
-			}	
-	}
-	print "------------------------------</pre></ol>
-	<a href=\"$global->{selfurl}?student=1&menu_=0\">To menu.</a>";
-}
 sub ToDB
 {
 	my $dbh=DBI->connect("DBI:mysql:mydata;localhost:3306", "root", "12345", {RaiseError=>1, AutoCommit=>1});
@@ -208,35 +225,5 @@ sub FromDB
 	}
 	$sth->finish();
 	$dbh ->disconnect();
-}
-
-sub tofile
-{	
-	dbmopen(my %hash,$Myfile,0644);
-	my $i=0;
-	my $s;
-	my @a=();
-	%hash=();
-	foreach my $item(@students)
-		{
-		if(defined $item->{type}){
-		 @a=("name",$item->{name},"surname",$item->{surname},"group",$item->{group},"age",$item->{age},"type",$item->{type});}
-		else { @a=("name",$item->{name},"surname",$item->{surname},"group",$item->{group},"age",$item->{age});}
-		$s=join(",",@a);
-		$hash{$i}=$s;
-		$i++;
-		}
-	dbmclose(%hash);	
-}
-
-sub fromfile
-{
-	dbmopen(my %hash,$Myfile,0644);
-		while (( my $key,my $value) = each(%hash))
-	{
-		my @st=split(/,/,$hash{$key});
-		$students[$key]={@st};
-	}
-	dbmclose(%hash);
 };
 1
